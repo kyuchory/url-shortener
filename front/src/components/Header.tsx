d497b24f78adb307d6dfa5722fce6dae9e70f554
kyuchory <sls789456@naver.com>
2025-03-30 19:12:00 +0900

    Feat: 헤더 간단하게 완성성

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,5 +1,19 @@
+import styles from "../styles/Header.module.css";
+import logo from "../assets/logo3.gif";
+
 const Header: React.FC = () => {
-  return <header>header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </div>
+      <nav className={styles.nav}>
+        <a href="/소개" className={styles.menuItem}>
+          Introduction
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
