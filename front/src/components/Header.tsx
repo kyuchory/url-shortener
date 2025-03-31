c99e11847c482af2c86cc938fa7748383770e2ed
kyuchory <sls789456@naver.com>
2025-03-31 19:06:17 +0900

    Feat: 헤더의 설명부분을 누르면 스크롤 가장 하단으로 이동

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -2,13 +2,20 @@ import styles from "../styles/Header.module.css";
 import logo from "../assets/logo3.gif";
 
 const Header: React.FC = () => {
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <img src={logo} alt="Logo" className={styles.logo} />
       </div>
       <nav className={styles.nav}>
-        <a href="/소개" className={styles.menuItem}>
+        <a onClick={scrollToBottom} className={styles.menuItem}>
           Introduction
         </a>
       </nav>
