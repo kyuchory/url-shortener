ff14e75975c9daf1895c773fff689568421ad491
kyuchory <sls789456@naver.com>
2025-03-31 18:42:30 +0900

    Feat: cors 설정 변경

--- back/server.js
+++ back/server.js
@@ -8,19 +8,13 @@ const PORT = process.env.PORT || 3001;
 
 // 미들웨어 설정
 app.use(express.json());
-app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // 라우터 연결
 app.use("/api", shortenerRoutes); // 모든 라우트를 `/api` 경로로 설정
